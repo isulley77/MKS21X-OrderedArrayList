24d8293cb9f0fa9c33ef31757aa0f1786e78be9c
isulley77 <isulley@stuy.edu>
2018-12-08 21:08:45 -0500

    added add @ index Method

--- NoNullArray.java
+++ NoNullArray.java
@@ -20,6 +20,13 @@ public class NoNullArray<T> extends ArrayList<T>{
         return super.add(val);
     
     }
+    
+    public void add(int index, T val){
+        if(val == null){
+            throw new IllegalArgumentException();
+        }
+        super.add(index, val);
+    }
 
 
     public T set(int index, T val){
